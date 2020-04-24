daac65cddec87070172459af2ee4206a9023ad57
zxiaoqi <mrzeng18229230299@163.com>
2020-04-24 12:14:07 +0800

    filter数据send处理

--- src/pages/found/index.js
+++ src/pages/found/index.js
@@ -16,15 +16,16 @@ class Found extends Component {
 			areaTitle: ["区域", "房型", "价格", "筛选"],
 			value: null,
 			activeIndex: null, //null
+			selectFilter: [[], [], [], []],
 			houseList: [],
-			houseCount:0
+			houseCount: 0,
 		};
 	}
 	componentDidMount() {
 		this.getCityId().then((res) => {
 			// console.log(res);
 			this.getAreaCity(res);
-			this.getHouseList(res)
+			this.getHouseList(res);
 		});
 	}
 	async getCityId() {
@@ -61,35 +62,76 @@ class Found extends Component {
 			filterlist,
 		});
 	}
-	async getHouseList(id) { 
+	async getHouseList(id) {
 		const { list, count } = (
 			await request.get("/houses?cityId=" + id)
 		).data.body;
 		// console.log(res);
 		this.setState({
 			houseList: list,
-			houseCount:count
-		})
-	}
-	onChange = (value) => {
-		console.log(value);
-		this.setState({
-			value,
+			houseCount: count,
 		});
-	};
-	onScrollChange = (value) => {
-		console.log(value);
-	};
+	}
 	//activerIndex改变
 	changeActive(activeIndex) {
 		this.setState({
 			activeIndex,
 		});
-		// this.renderActivePicker();
 	}
+	//Picker选中
+	onChange = (value) => {
+		// console.log(value);
+		const { selectFilter, activeIndex } = this.state;
+		selectFilter[activeIndex] = value;
+		this.setState({
+			selectFilter,
+		});
+	};
+	//选中
+	handleSelectFilter(val) {
+		const { selectFilter, activeIndex } = this.state;
+		let index = selectFilter[activeIndex].findIndex((v) => v === val);
+		if (index === -1) {
+			selectFilter[activeIndex].push(val);
+		} else {
+			selectFilter[activeIndex].splice(index, 1);
+		}
+		this.setState({
+			selectFilter,
+		});
+	}
+
+	//确定选择
+	handleSubmit = () => {
+		const { selectFilter } = this.state;
+		const areaOrSubway = selectFilter[0][0],
+			rentType = selectFilter[1][0],
+			price = selectFilter[2][0],
+			more = selectFilter[3].join(',');
+		//判断区域属性值
+		let areaOrSubway_val = ["null", undefined].includes(selectFilter[0][2])
+			? selectFilter[0][1]
+			: selectFilter[0][2];
+		//构建对象数据
+		let obj = {
+			[areaOrSubway]: areaOrSubway_val,
+			rentType,
+			price,
+			more,
+		};
+		//过滤不合理的属性
+		for (const key in obj) {
+			if (["null","",undefined].includes(obj[key])) {
+				delete obj[key]
+			}
+		}
+		console.log(obj);
+		
+
+	};
 	//picker结构
 	renderActivePicker() {
-		const { filterlist, activeIndex } = this.state;
+		const { filterlist, activeIndex, selectFilter } = this.state;
 		if (filterlist.length == 0) {
 			return null;
 		} else if (activeIndex >= 0 && activeIndex <= 2) {
@@ -97,8 +139,7 @@ class Found extends Component {
 				<div className={Css.filter_box}>
 					<PickerView
 						onChange={this.onChange}
-						onScrollChange={this.onScrollChange}
-						value={this.state.value}
+						value={selectFilter[activeIndex]}
 						data={filterlist[activeIndex]}
 						cascade={activeIndex == 0 ? true : false}
 					/>
@@ -106,7 +147,9 @@ class Found extends Component {
 						<div className={Css.cancel} onClick={this.hideMask}>
 							取消
 						</div>
-						<div className={Css.deter}>确定</div>
+						<div className={Css.deter} onClick={this.handleSubmit}>
+							确定
+						</div>
 					</div>
 				</div>
 			);
@@ -119,7 +162,18 @@ class Found extends Component {
 								<div className={Css.item_title}>{v.label}</div>
 								<div className={Css.item_tags}>
 									{v.children.map((item) => (
-										<div className={Css.tag} key={item.value}>
+										<div
+											className={[
+												Css.tag,
+												selectFilter[activeIndex].findIndex(
+													(v) => v === item.value
+												) !== -1
+													? Css.tag_active
+													: "",
+											].join(" ")}
+											key={item.value}
+											onClick={() => this.handleSelectFilter(item.value)}
+										>
 											{item.label}
 										</div>
 									))}
@@ -131,7 +185,9 @@ class Found extends Component {
 						<div className={Css.cancel} onClick={this.hideMask}>
 							清除
 						</div>
-						<div className={Css.deter}>确定</div>
+						<div className={Css.deter} onClick={this.handleSubmit}>
+							确定
+						</div>
 					</div>
 				</div>
 			);
@@ -150,29 +206,27 @@ class Found extends Component {
 	}
 	//list-item结构
 	rowRenderer = ({ key, index, isScrolling, isVisible, style }) => {
-		const { houseList } =this.state
-		return (
-			houseList.map((v) => (
-				<div key={v.houseCode} className={Css.item_content}>
-					<div className={Css.list_img}>
-						<img src={baseURL + v.houseImg} />
-					</div>
-					<div className={Css.item_house}>
-						<div className={Css.house_name}>{v.title}</div>
-						<div className={Css.house_desc}>{v.desc}</div>
-						{v.tags.map((tag) => (
-							<span key={tag} className={Css.tags}>
-								{tag}
-							</span>
-						))}
-						<div className={Css.price}>
-							<span>{v.price}</span>
-							元/月
-						</div>
+		const { houseList } = this.state;
+		return houseList.map((v) => (
+			<div key={v.houseCode} className={Css.item_content}>
+				<div className={Css.list_img}>
+					<img src={baseURL + v.houseImg} />
+				</div>
+				<div className={Css.item_house}>
+					<div className={Css.house_name}>{v.title}</div>
+					<div className={Css.house_desc}>{v.desc}</div>
+					{v.tags.map((tag) => (
+						<span key={tag} className={Css.tags}>
+							{tag}
+						</span>
+					))}
+					<div className={Css.price}>
+						<span>{v.price}</span>
+						元/月
 					</div>
 				</div>
-			))
-		);
+			</div>
+		));
 	};
 	render() {
 		const { areaTitle, activeIndex, houseList } = this.state;
